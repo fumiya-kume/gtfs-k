40b9eba014e1ba0f1ab0bd0e40b1eb9c5c627ea0
fumiya-kume <fumiya.kume@hotmail.com>
2024-10-19 21:49:37 +0900

    Update Example module

--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -5,5 +5,13 @@ import io.github.fumiya_kume.gtfs_k.lib.gtfsReader
 fun main() {
     val gtfsUrl = "https://github.com/fumiya-kume/gtfs-k/raw/refs/heads/master/test-data/toyotetsu.zip"
     val result = gtfsReader(gtfsUrl)
-    print(result.agency)
+    result.agency.forEach {
+        println(it.agencyName)
+        println(it.agencyLang)
+    }
+    result.agencyJapan.forEach {
+        println(it.agencyId)
+        println(it.agencyAddress)
+        println(it.agencyZipCode)
+    }
 }
